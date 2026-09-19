16031ff042a27863a63d445701945f143e094aa1
Sanjida Akter Sima <simasanjida8@gmail.com>
2026-09-20 03:40:11 +0600

    added another button

--- src/componenet/countries/Countries.tsx
+++ src/componenet/countries/Countries.tsx
@@ -1,4 +1,4 @@
-import { use } from "react";
+import { use, useState } from "react";
 import type { CountryType } from "../Type";
 import Country from "../country/Country";
 import "./Countries.css";
@@ -7,16 +7,25 @@ export interface CountriesProps {
 }
 
 export default function Countries({ countriesPromise }: CountriesProps) {
-  const countries = use(countriesPromise);
 
+const [visitedCountries, setVisitedCountries] = useState<CountryType[]>([]);
+
+  const countries = use(countriesPromise);
+  const handleVisitedCountries = (Country: CountryType): void =>{
+    const newVisitedCountries = [...visitedCountries, Country];
+    setVisitedCountries(newVisitedCountries)
+  }
   return (
     <div>
           <h1>countries : {countries.length}</h1>
+          <h3>Visited Countries: {visitedCountries.length}</h3>
       <div className="countries">
       
         
         {countries.map(country => 
-          <Country key={country.ccn3.ccn3} country={country}></Country>
+          <Country key={country.ccn3.ccn3} country={country}
+          handleVisitedCountries = {handleVisitedCountries}
+          ></Country>
         )
         }
         
--- src/componenet/country/Country.tsx
+++ src/componenet/country/Country.tsx
@@ -3,9 +3,9 @@ import type { CountryType } from "../Type";
 import "./Country.css";
 export interface CountryProps {
   country: CountryType;
+  handleVisitedCountries:(country: CountryType)=> void
 }
-
-export default function Country({ country }: CountryProps) {
+export default function Country({ country, handleVisitedCountries }: CountryProps) {
 
  const [visited , setVisited] = useState<boolean>(false)
 const handleVisited = () =>{
@@ -15,29 +15,20 @@ const handleVisited = () =>{
   // }
   // else{
   //   setVisited(true)
-
   // }
-
   setVisited(!visited)
+  handleVisitedCountries(country)
 }
-
   return (
     <div className={`country ${visited ? 'country-visited' :'mark as visited' }`}>
-
       <h3>{country.name.common}</h3>
-
       <img src={country.flags.flags.png} alt={country.name.common} />
-
       <p> Population: {country.population.population}</p>
-
       <p>continents: {country.continents.continents}</p>
-
       <p>capital: {country.capital.capital}</p>
-
       <button onClick={handleVisited}>
         { visited? 'Visited':'mark as Visited' }
         </button>
-
     </div>
   );
 }
